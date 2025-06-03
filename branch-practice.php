592c3b43b503875893f243e63f18742bb2324fd9
YK8201 <kd1382288@st.kobedenshi.ac.jp>
2025-06-03 14:29:35 +0900

    Update branch-practice.php
    出力内容変更(こんにちは世界→ブランチ作成練習)

---
+++ branch-practice.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8" />
+        <title>サンプルサイト</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        <?php
+            print("ブランチ作成練習");
+        ?>
+    </body>
+</html>
